4fb5faecddb405aa5ff80fcaa8ef19be340f46bc
nothingtosay920 <963717822@qq.com>
2021-12-16 22:36:08 +0800

    feat: 列表优化

--- src/uills/pageList.js
+++ src/uills/pageList.js
@@ -1,51 +1,48 @@
 "use strict";
-// const rounding = (num: number) => {
-//   if (Math.trunc(num) < num) {
-//     return Math.trunc(num) + 1
-//   }
-//     return num 
-// }
 exports.__esModule = true;
 exports.usePageList = void 0;
-// const getGroupLength = (arr: any[] | undefined, groupsItemNum: number) => {
-//   if (!arr) return 0
-//   return rounding(arr.length / groupsItemNum)
-// }
-var transFromPageList = function (arr, page, groupsItemNum) {
-    if (page === void 0) { page = 0; }
+var rounding = function (num) {
+    if (Math.trunc(num) < num) {
+        return Math.trunc(num) + 1;
+    }
+    return num;
+};
+var getIistNums = function (arr, page, options) { return arr.slice(0, (page + 1) * options.groupsItemNum); };
+var getItemsNums = function (groupsItemNum, groupsNums) { return rounding(groupsItemNum / groupsNums); };
+var addObjList = function (arr, options) {
+    var res = [];
+    var i = 0;
+    var itemNums = getItemsNums(options.groupsItemNum, options.groupsNums);
+    while (i < rounding(arr.length / options.groupsItemNum)) {
+        var obj = {};
+        var array = arr.slice(options.groupsItemNum * i, options.groupsItemNum * (i + 1));
+        if (!array.length)
+            return res;
+        var li = [];
+        for (var j = 0; j < options.groupsNums; j++) {
+            if (array.slice(itemNums * j, itemNums * (j + 1)).length) {
+                li.push(array.slice(itemNums * j, itemNums * (j + 1)));
+            }
+        }
+        obj['id'] = i;
+        obj['list'] = li;
+        res.push(obj);
+        i++;
+    }
+    return res;
+};
+var transFromPageList = function (arr, options) {
     if (!arr)
         return [];
-    var res = [];
-    console.log((page + 1) * groupsItemNum);
-    var obj = {};
-    var list = arr.slice(0, (page + 1) * groupsItemNum);
-    // for (var i = 0; i < (page + 1) * groupsItemNum / 20; i++) {
-    //     res.push(list.slice(20 * i, 20 * (i + 1)));
-    // }
-    let i = 0
-    while (i < arr.length) {
-        res.push(list.slice(i, Math.min(i+20, arr.length)))
-        i += 20
-    }
-    obj['pageNums'] = arr.length / groupsItemNum;
-    obj['arr'] = res;
-    obj['id'] = page;
-    // arr.slice(0, page * groupsItemNum + 1)
-    return obj;
+    return addObjList(arr, options);
 };
-exports.usePageList = function (arr, page, groupsItemNum) {
-    if (groupsItemNum === void 0) { groupsItemNum = 100; }
-    return transFromPageList(arr, page, groupsItemNum);
+var usePageList = function (arr, page, options) {
+    if (options === void 0) { options = { groupsItemNum: 100, groupsNums: 20 }; }
+    if (!arr)
+        return [];
+    var listNums = getIistNums(arr, page, options);
+    return transFromPageList(listNums, options);
 };
-console.log(exports.usePageList([1, 2, 3, 4, 5, 6, 7, 8,
-     1, 2, 3, 4, 5, 6, 7, 8, 
-     1, 2, 3, 4, 5, 6, 7, 8,
-      1, 2, 3, 4, 5, 6, 7, 81,
-       2, 3, 4, 5, 6, 7, 81, 2,
-        3, 4, 5, 6, 7, 81, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8], 0));
-
-// 0 19
-// 20 39
-// 40 59
-// 80 99
-//
+exports.usePageList = usePageList;
+console.log(usePageList([1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8], 0));
+console.log([1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8].length);
--- src/uills/pageList.ts
+++ src/uills/pageList.ts
@@ -1,49 +1,52 @@
-// const rounding = (num: number) => {
-//   if (Math.trunc(num) < num) {
-//     return Math.trunc(num) + 1
-//   }
-//     return num 
-// }
+const rounding = (num: number) => {
+  if (Math.trunc(num) < num) {
+    return Math.trunc(num) + 1
+  }
+    return num 
+}
 
 // const getGroupLength = (arr: any[] | undefined, groupsItemNum: number) => {
 //   if (!arr) return 0
 //   return rounding(arr.length / groupsItemNum)
 // }
 type pageListOptions = {
-  page: number,
-  groupsItemNum: number,
-  groupsNums: number
+  groupsItemNum: number,  // 全部数组中的项数之和
+  groupsNums: number // 数组内对象数
 }
-const getIistNums = (options: pageListOptions) => (options.page + 1) * options.groupsItemNum
-const getPageData = (arr: any[], nums: number) => arr.slice(0, nums)
-const addObjList = (arr: any[], nums: number) => {
+const getIistNums = (arr: any[], page: number, options: pageListOptions) => arr.slice(0, (page + 1) * options.groupsItemNum)
+const getItemsNums = (groupsItemNum: number, groupsNums: number) => rounding(groupsItemNum / groupsNums)
+const addObjList = (arr: any[], options: pageListOptions) => {
   const res = []
-  for (let i = 0; i < nums / 20; i++) {
-    res.push(arr.slice(20 * i, 20 * (i+1)))
+  let i = 0
+  const itemNums = getItemsNums(options.groupsItemNum, options.groupsNums)
+  while (i < rounding(arr.length / options.groupsItemNum) ) {
+    const obj = {}
+    const array = arr.slice(options.groupsItemNum * i, options.groupsItemNum * (i + 1))
+    if (!array.length) return res
+    const li = []
+    for (let j =  0; j < options.groupsNums; j++) {
+      if (array.slice(itemNums * j, itemNums * (j + 1)).length) {
+        li.push(array.slice( itemNums * j, itemNums * (j+1)))
+      }
+    }
+    obj['id'] = i
+    obj['list'] = li
+    res.push(obj)
+    i++
   }
   return res
 }
 
-const parseObj = () => {
-  
-}
-
-const transFromPageList = (arr: any[] | undefined, nums: number) => {
+const transFromPageList = (arr: any[] | undefined, options: pageListOptions) => {
   if (!arr) return []
-  const obj = {}
-  const list = getPageData(arr, nums)
-  getPageData(getPageData(arr, nums), nums)
-  obj['pageNums'] = arr.length / groupsItemNum
-  obj['arr'] = res
-  obj['id'] = page
-  // arr.slice(0, page * groupsItemNum + 1)
-  return obj
+  return addObjList(arr, options)
 }
 
 export const usePageList = 
-  (arr: any[] | undefined, options: pageListOptions) => {
-    const listNums = getIistNums(options)
-    return transFromPageList(arr, listNums)
+  (arr: any[] | undefined, page: number, options: pageListOptions = { groupsItemNum: 100, groupsNums: 2 }) => {
+    if(!arr) return []
+    const listNums = getIistNums(arr, page, options)
+    return transFromPageList(listNums, options)
   }
 
-console.log(usePageList([1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8], 0));
+// console.log(usePageList([1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,81,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8], 0));
